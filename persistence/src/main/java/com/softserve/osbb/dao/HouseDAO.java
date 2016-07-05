1bb783986d6c1271074f1bdc573fcb1e2257e90e
NazDov <nazardovhuy@gmail.com>
2016-07-05 20:01:45 +0300

    changed key type to Integer

--- persistence/src/main/java/com/softserve/osbb/dao/HouseDAO.java
+++ persistence/src/main/java/com/softserve/osbb/dao/HouseDAO.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface HouseDAO extends JpaRepository<HouseEntity, Long> {
+public interface HouseDAO extends JpaRepository<HouseEntity, Integer> {
 }
