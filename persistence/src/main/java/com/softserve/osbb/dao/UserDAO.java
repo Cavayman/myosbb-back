481e93a059850a1d0a495b09ed894075ec8846b4
Cavayman <cavayman@gmail.com>
2016-07-05 17:44:35 +0300

    finished UserDAO

--- persistence/src/main/java/com/softserve/osbb/dao/UserDAO.java
+++ persistence/src/main/java/com/softserve/osbb/dao/UserDAO.java
@@ -1,7 +1,12 @@
 package com.softserve.osbb.dao;
 
+import com.softserve.osbb.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /**
  * Created by cavayman on 05.07.2016.
  */
-public class UserDAO {
+@Repository
+public interface UserDAO extends JpaRepository<UserEntity,Integer> {
 }
