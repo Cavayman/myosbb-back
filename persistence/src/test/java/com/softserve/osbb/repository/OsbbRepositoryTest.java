a67948036ca72435316558571a021193dc72a301
butaroman <butaromanjob@gmail.com>
2016-07-06 15:42:52 +0300

    OsbbRepositoryTest.java was changed.Were added some methods.

--- persistence/src/test/java/com/softserve/osbb/repository/OsbbRepositoryTest.java
+++ persistence/src/test/java/com/softserve/osbb/repository/OsbbRepositoryTest.java
@@ -11,6 +11,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Roman on 06.07.2016.
  */
@@ -32,11 +35,49 @@ public class OsbbRepositoryTest {
         osbbEntity.setCreatorId(5);
     }
 
+    @Before
+    public void addIntoDB() {
+        osbbEntity = osbbRepository.save(osbbEntity);
+    }
+
     @Test
     public void testSave(){
         OsbbEntity savedOsbb = osbbRepository.save(osbbEntity);
         Assert.assertNotNull(savedOsbb);
-        Assert.assertNotEquals(0, (long)osbbEntity.getOsbbId());
-        Assert.assertEquals("Lviv_osbb",osbbEntity.getName());
+        Assert.assertEquals("Lviv_osbb",savedOsbb.getName());
     }
+
+    @Test
+    public void testGetOsbbById() {
+        OsbbEntity savedOsbb = osbbRepository.save(osbbEntity);
+        Assert.assertEquals(osbbRepository.getOne(savedOsbb.getOsbbId()),savedOsbb);
+    }
+
+    @Test
+    public void testGetAllOsbb() {
+        List<OsbbEntity> list = Arrays.asList(new OsbbEntity(), new OsbbEntity(), new OsbbEntity());
+        osbbRepository.deleteAll();
+        osbbRepository.save(list);
+        Assert.assertTrue(list.size() == osbbRepository.findAll().size());
+    }
+
+    @Test
+    public void testDeleteOsbbById() {
+        osbbRepository.delete(osbbEntity.getOsbbId());
+        Assert.assertFalse(osbbRepository.exists(osbbEntity.getOsbbId()));
+    }
+
+    @Test
+    public void testDeleteOsbbByOsbbEntity() {
+        osbbRepository.delete(osbbEntity);
+        Assert.assertFalse(osbbRepository.exists(osbbEntity.getOsbbId()));
+    }
+
+    @Test
+    public void testDeleteAllOsbb() {
+        Assert.assertNotEquals(0, osbbRepository.findAll().size());
+        osbbRepository.deleteAll();
+        Assert.assertEquals(0, osbbRepository.findAll().size());
+    }
+
 }
