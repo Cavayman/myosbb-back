9ea74fe68040c7a9f0f80ee400f663e4262b991c
NazDov <nazardovhuy@gmail.com>
2016-07-09 02:21:23 +0300

    adding ReportService

--- service/src/main/java/com/softserve/osbb/service/ReportService.java
+++ service/src/main/java/com/softserve/osbb/service/ReportService.java
@@ -2,6 +2,7 @@ package com.softserve.osbb.service;
 
 import com.softserve.osbb.model.Report;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -11,10 +12,22 @@ public interface ReportService {
 
     Report addReport(Report report);
 
-    Report getReportById(int reportId);
+    Report getReportById(Integer reportId);
 
     Report getReportByName(String name);
 
+    List<Report> getAllReportsBySearchTerm(String searchTerm);
+
+    List<Report> getAllReportsBetweenDates(LocalDateTime from, LocalDateTime to);
+
     List<Report> getAllReports();
+
+    List<Report> showLatestRepors();
+
+    void deleteAll();
+
+    void deleteReportById(Integer reportId);
+
+
 
 }
