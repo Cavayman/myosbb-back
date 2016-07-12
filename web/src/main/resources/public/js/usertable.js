d0acd19af1de3ce3dd32434fc14eecda10783a96
Cavayman <cavayman@gmail.com>
2016-07-13 00:31:43 +0300

    added my  js with  jquery. Will update

---
+++ web/src/main/resources/public/js/usertable.js
@@ -0,0 +1,32 @@
+
+$("#userAdd").click(function () {
+alert();
+});
+
+function show() {
+    // var input=$('#focusedInput').val();
+    // $('#tablecont').html("");
+    // $.ajax({
+    //     url : "Allcity/getAll-"+input,
+    //
+    //     type : 'GET',
+    //     success : function(data) {
+    //
+    //         for ( var index in data) {
+    //             var tr = $('<tr>');
+    //             var tdId = $('<td>').text(data[index].id);
+    //             var tdName = $('<td>').text(data[index].name);
+    //             var tdEmail = $('<td>').text(data[index].email);
+    //             var button = $('<button>').attr("class","btn btn-info transfed")
+    //                 .attr("data-toggle","modal")
+    //                 .attr("data-target","#myModal")
+    //                 .attr('id', data[index].id).text("transer");
+    //             button.click(modal);
+    //             var tdButton=$('<td>').append(button);
+    //             tr.append(tdId).append(tdName).append(tdEmail).append(tdButton);
+    //             $('#tablecont').append(tr);
+    //         }
+    //     }
+    // });
+alert('asd');
+};
