ffec65cf7d9788375bae16c1c026f9a1761a8317
Aska <aska.fed@gmail.com>
2016-07-13 18:09:49 +0300

    fix error handling in provider controller

---
+++ web/src/main/java/com/softserve/osbb/util/EntityNotFoundException.java
@@ -0,0 +1,25 @@
+package com.softserve.osbb.util;
+
+/**
+ * Created by Anastasiia Fedorak on 13.07.2016.
+ */
+public class EntityNotFoundException extends Exception {
+    public EntityNotFoundException() {
+    }
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EntityNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
