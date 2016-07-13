9118293e24084bc5afad4379f18fc0f1de9e075c
NazDov <nazardovhuy@gmail.com>
2016-07-13 14:14:45 +0300

    updated validation

--- persistence/src/test/java/com/softserve/osbb/repository/ReportRepositoryTest.java
+++ persistence/src/test/java/com/softserve/osbb/repository/ReportRepositoryTest.java
@@ -16,6 +16,7 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -37,7 +38,6 @@ public class ReportRepositoryTest {
 
     @Before
     public void init() {
-
         report = new Report();
         LocalDate dateCreation = LocalDate.now();
         report = new Report();
@@ -48,65 +48,68 @@ public class ReportRepositoryTest {
 
     @Test
     public void testSaveReport() {
-
         report = reportRepository.save(report);
-
         assertNotNull(report);
-
         assertTrue(reportRepository.exists(report.getReportId()));
-
     }
 
     @Test
     public void testUpdateReport() {
-
         report = reportRepository.save(report);
-
         report.setName("баланс ЧЕРВ/2016 2.0");
-
         Report updatedReport;
         updatedReport = reportRepository.saveAndFlush(report);
-
         assertSame(report.getReportId(), updatedReport.getReportId());
     }
 
 
     @Test
     public void testDeleteReport() {
-
         report = reportRepository.save(report);
-
         reportRepository.delete(report);
-
         assertFalse(reportRepository.exists(report.getReportId()));
-
-
     }
 
     @Test
     public void testGetAllReports() {
-
-               List<Report> reportEntities = new ArrayList<>();
-
+        List<Report> reportEntities = new ArrayList<>();
         reportEntities.add(new Report("баланс ЛИП/2016", "фін. звіт за липень"));
         reportEntities.add(new Report("баланс СЕР/2016", "фін. звіт за серпень"));
         reportEntities.add(new Report("баланс ВЕР/2016", "фін. звіт за вересень"));
-
-        reportRepository.save(reportEntities);
-
-        assertTrue(reportRepository.findAll().size() != reportEntities.size());
+        List<Report> savedReports = reportRepository.save(reportEntities);
+        assertArrayEquals(reportEntities.toArray(new Report[reportEntities.size()]),
+                savedReports.toArray(new Report[savedReports.size()]));
         System.out.println(reportRepository.findAll().size());
     }
 
     @Test
     public void testDeleteAllReports() {
-
         reportRepository.deleteAll();
-
         assertTrue(reportRepository.findAll().isEmpty());
 
     }
 
+    @Test
+    public void testGetAllReportsByYear() {
+        List<Report> reportList = new ArrayList<>();
+        Report reportOne = new Report();
+        reportOne.setName("report1");
+        reportOne.setCreationDate(LocalDate.now().minusYears(2));
+        reportList.add(reportOne);
+        Report reportTwo = new Report();
+        reportTwo.setName("report2");
+        reportTwo.setCreationDate(LocalDate.now().minusYears(1));
+        reportList.add(reportTwo);
+        Report reportThree = new Report();
+        reportThree.setName("report3");
+        reportThree.setCreationDate(LocalDate.now().minusYears(2));
+        reportList.add(reportThree);
+
+        reportRepository.save(reportList);
+
+        List<Report> reportsByYear = reportRepository.findByCreationDate(LocalDate.now().minusYears(2));
+        assertTrue(reportsByYear.size() == 2);
+    }
 
 
 }
