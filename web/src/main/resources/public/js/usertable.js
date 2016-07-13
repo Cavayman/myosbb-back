db2e3423f821aacd1c5999a05bfefb2bc7fe6d25
Cavayman <cavayman@gmail.com>
2016-07-13 03:05:25 +0300

    added deleting

--- web/src/main/resources/public/js/usertable.js
+++ web/src/main/resources/public/js/usertable.js
@@ -1,60 +1,43 @@
-$(function(){
+$(document).ready(show);
+function show() {
     $('#tablecont').html("");
     $.ajax({
-        url : "user/",
-        type : 'GET',
-        success : function(data) {
-            for ( var index in data) {
+        url: "user/",
+        type: 'GET',
+        success: function (data) {
+            for (var index in data) {
                 var tr = $('<tr>');
                 var tdId = $('<td>').text(data[index].userId);
-                var tdfName = $('<td>').text(data[index].firstName).attr("class","small");
-                var tdlName = $('<td>').text(data[index].lastName).attr("class","small");
-                var tdbDate = $('<td>').text(data[index].birthDate).attr("class","small");
-                var tdEmail = $('<td>').text(data[index].email).attr("class","small");
-                var tdPhoneN = $('<td>').text(data[index].phoneNumber).attr("class","small");
-                var tdPass = $('<td>').text(data[index].password).attr("class","small");
-                var tdGender = $('<td>').text(data[index].gender).attr("class","small");
+                var tdfName = $('<td>').text(data[index].firstName).attr("class", "small");
+                var tdlName = $('<td>').text(data[index].lastName).attr("class", "small");
+                var tdbDate = $('<td>').text(new Date(data[index].birthDate).toLocaleDateString(['ban', 'id'])).attr("class", "small");
+                var tdEmail = $('<td>').text(data[index].email).attr("class", "small");
+                var tdPhoneN = $('<td>').text(data[index].phoneNumber).attr("class", "small");
+                var tdPass = $('<td>').text(data[index].password).attr("class", "small");
+                var tdGender = $('<td>').text(data[index].gender).attr("class", "small");
                 var button = $('<button>')
-                    .attr("class","btn btn-success btn-sm").text("Edit").val("bId",data[index].toString());
+                    .attr("class", "btn btn-success btn-sm")
+                    .text("Edit").attr("bid", data[index].userId.toString())
+                    .attr('id', data[index].userId)
+                    .click(deleteById);
                 var sbutton = $('<button>')
-                    .attr("class","btn btn-warning btn-sm").text("Delete",data[index].toString());
+                    .attr("class", "btn btn-warning btn-sm").text("Delete").attr("bid", data[index].userId.toString());
 
-                var tdButton=$('<td>').append(button);
-                var tdSButton=$('<td>').append(sbutton);
+                var tdButton = $('<td>').append(button);
+                var tdSButton = $('<td>').append(sbutton);
                 tr.append(tdId).append(tdfName).append(tdlName).append(tdbDate)
                     .append(tdEmail).append(tdPhoneN).append(tdPass).append(tdGender).append(tdButton).append(tdSButton);
                 $('#tablecont').append(tr);
             }
         }
-    });
-});
+    })
+};
 
-$("#userAdd").click();
-
-function show() {
-    var input=$('#focusedInput').val();
-    $('#tablecont').html("");
+function deleteById() {
     $.ajax({
-        url : "Allcity/getAll-"+input,
+        url: "user/" + $(this).attr('id'),
 
-        type : 'GET',
-        success : function(data) {
-
-            for ( var index in data) {
-                var tr = $('<tr>');
-                var tdId = $('<td>').text(data[index].id);
-                var tdName = $('<td>').text(data[index].name);
-                var tdEmail = $('<td>').text(data[index].email);
-                var button = $('<button>').attr("class","btn btn-info transfed")
-                    .attr("data-toggle","modal")
-                    .attr("data-target","#myModal")
-                    .attr('id', data[index].id).text("transer");
-                button.click(modal);
-                var tdButton=$('<td>').append(button);
-                tr.append(tdId).append(tdName).append(tdEmail).append(tdButton);
-                $('#tablecont').append(tr);
-            }
-        }
-    });
-alert('asd');
+        type: 'DELETE',
+    })
+    show();
 };
