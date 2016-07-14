bd853e166acf046cca8b857659d1a7d0ad251b1e
nkharabaruk <nkharabaruk@gmail.com>
2016-07-14 17:01:31 +0300

    liquibase - put relations in the entities changelogs

--- persistence/src/main/java/com/softserve/osbb/repository/EventRepository.java
+++ persistence/src/main/java/com/softserve/osbb/repository/EventRepository.java
@@ -1,15 +1,9 @@
 package com.softserve.osbb.repository;
 
 import com.softserve.osbb.model.Event;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.List;
 
 
 @Repository
