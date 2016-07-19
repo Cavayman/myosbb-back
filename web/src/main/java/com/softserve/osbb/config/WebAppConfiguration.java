343e12019d9bcf0a17418c85efe4b64f710d4d97
Cavayman <cavayman@gmail.com>
2016-07-19 18:18:43 +0300

    finished to restore from deleted stash

--- web/src/main/java/com/softserve/osbb/config/WebAppConfiguration.java
+++ web/src/main/java/com/softserve/osbb/config/WebAppConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * Created by nazar.dovhyy on 08.07.2016.
  */
 @SpringBootApplication
-@Import({PersistenceConfiguration.class/*,SecurityWebApplicationInitializer.class*/})
+@Import(PersistenceConfiguration.class)
 @ComponentScan(basePackages = {"com.softserve.osbb"})
 public class WebAppConfiguration extends WebMvcConfigurerAdapter {
 
@@ -35,7 +35,7 @@ public class WebAppConfiguration extends WebMvcConfigurerAdapter {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(new Object[]{WebAppConfiguration.class
+        SpringApplication.run(new Object[]{WebAppConfiguration.class,SecurityConfig.class
                 }, args);
     }
 
