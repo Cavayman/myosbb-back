a51f41568fe03f17862d0b69289d0b1eff0a3e71
aska <aska.fed@gmail.com>
2016-07-20 17:41:22 +0300

    rename provider type to provider periodicity

---
+++ persistence/src/main/java/com/softserve/osbb/model/enums/ProviderPeriodicity.java
@@ -0,0 +1,12 @@
+package com.softserve.osbb.model.enums;
+
+/**
+ * Created by Anastasiia Fedorak on 7/18/16.
+ */
+public enum ProviderPeriodicity {
+    ONE_TIME,
+    PERMANENT_DAYLY,
+    PERMANENT_WEEKLY,
+    PERMANENT_MONTHLY,
+    PERMANENT_YEARLY,
+}
