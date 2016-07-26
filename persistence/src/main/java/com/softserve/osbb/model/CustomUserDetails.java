f743624c29e7e0a4914863f4385c2f84b9f7df96
nataliia <nkharabaruk@gmail.com>
2016-07-26 15:16:02 +0300

    returned to Spring Boot Tomcat and jar files

--- persistence/src/main/java/com/softserve/osbb/model/CustomUserDetails.java
+++ persistence/src/main/java/com/softserve/osbb/model/CustomUserDetails.java
@@ -1,50 +1,48 @@
-//package com.softserve.osbb.model;
-//
-//import com.softserve.osbb.model.User;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.AuthorityUtils;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.Collection;
-//
-///**
-// * Created by cavayman on 19.07.2016.
-// */
-//
-//public class CustomUserDetails extends User implements UserDetails {
-//
-//    public CustomUserDetails(User user){
-//        super(user);
-//    };
-//
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return AuthorityUtils.createAuthorityList("ROLE_USER");
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//    return getEmail();
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return true;
-//    }
-//}
+package com.softserve.osbb.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * Created by cavayman on 19.07.2016.
+ */
+
+public class CustomUserDetails extends User implements UserDetails {
+
+    public CustomUserDetails(User user){
+        super(user);
+    };
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.createAuthorityList("ROLE_USER");
+    }
+
+    @Override
+    public String getUsername() {
+    return getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
