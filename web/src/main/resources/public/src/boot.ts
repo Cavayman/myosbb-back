e23e123701f6613f4b9bdc1a58fbaf238e37fd42
aska <aska.fed@gmail.com>
2016-08-02 13:39:21 +0300

    fix bug with missed field

--- web/src/main/resources/public/src/boot.ts
+++ web/src/main/resources/public/src/boot.ts
@@ -1,25 +1,27 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
-import {ROUTER_DIRECTIVES} from '@angular/router';
-import {AppComponent} from "./app/app.component";
-import {APP_ROUTER_PROVIDERS} from "./app/app.routes";
-import {disableDeprecatedForms, provideForms} from '@angular/forms';
 import {HTTP_PROVIDERS, Http} from '@angular/http'
+import {ROUTER_DIRECTIVES} from '@angular/router';
+import {APP_ROUTER_PROVIDERS} from "./app/app.routes";
 import {AppComponent} from "./app/app.component";
+import {disableDeprecatedForms, provideForms} from '@angular/forms';
 import {TranslateService, TranslateLoader, TranslateStaticLoader} from "ng2-translate/ng2-translate";
 
 bootstrap(AppComponent,
-    [HTTP_PROVIDERS,
-        APP_ROUTER_PROVIDERS,
-        ROUTER_DIRECTIVES,
-        disableDeprecatedForms(),
-        provideForms(),
-        // not required, but recommended to have 1 unique instance of your service
-        {
-            provide: TranslateLoader,
-            useFactory: (http: Http) => new TranslateStaticLoader(http, 'assets/i18n', '.json'),
-            deps: [Http]
-        },
+[HTTP_PROVIDERS, APP_ROUTER_PROVIDERS,
+ROUTER_DIRECTIVES,
+disableDeprecatedForms(),
+provideForms(),
 
-        // use TranslateService here, and not TRANSLATE_PROVIDERS (which will define a default TranslateStaticLoader)
-        TranslateService])
-    .catch(err => console.error(err));
+// not required, but recommended to have 1 unique instance of your service
+{
+provide: TranslateLoader,
+useFactory: (http: Http) => new TranslateStaticLoader(http, 'assets/i18n', '.json'),
+deps: [Http]
+},
+
+// use TranslateService here, and not TRANSLATE_PROVIDERS (which will define a default TranslateStaticLoader)
+TranslateService]);
+
+//    .catch(err => console.error(err));
+
+
