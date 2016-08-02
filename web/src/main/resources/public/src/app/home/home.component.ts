348a94b6134ce250114cd706eaabdc3d04608c66
NazDov <nazardovhuy@gmail.com>
2016-08-03 01:39:12 +0300

    LVJAVAAA-94 adding user profile link to the navar

--- web/src/main/resources/public/src/app/home/home.component.ts
+++ web/src/main/resources/public/src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core'
 import {ROUTER_DIRECTIVES} from '@angular/router'
 import {UserComponent} from "../user/user.component";
 import {HeaderComponent} from "././header/header.component";
-import {SidebarMenuComponent} from "../sidebar_menu/sidebar.menu.component";
+import {SidebarMenuComponent} from "../home/sidebar_menu/sidebar_menu.component";
 
 @Component({
     selector: 'app-home',
--- web/src/main/resources/public/src/app/user/user.component.ts
+++ web/src/main/resources/public/src/app/user/user.component.ts
@@ -3,11 +3,12 @@ import {SidebarMenuComponent} from "../sidebar_menu/sidebar.menu.component";
 import {UserMainComponent} from "./main/user.main.component";
 import {ROUTER_DIRECTIVES} from "@angular/router";
 import {UserHouseComponent} from "./house/user.house.component";
+import {HeaderComponent} from "../header/header.component";
 
 @Component({
     selector: 'my-user',
     templateUrl: 'src/app/user/user.html',
-    directives: [ROUTER_DIRECTIVES, SidebarMenuComponent, UserMainComponent, UserHouseComponent],
+    directives: [ROUTER_DIRECTIVES, HeaderComponent, SidebarMenuComponent, UserMainComponent, UserHouseComponent],
     outputs: ['userName']
 })
 export class UserComponent {
