1e6b0b4ce1cea1fdfda58faad8db1332ad209640
NazDov <nazardovhuy@gmail.com>
2016-08-03 16:23:11 +0300

    fixing errors

--- web/src/main/java/com/softserve/osbb/controller/ReportController.java
+++ web/src/main/java/com/softserve/osbb/controller/ReportController.java
@@ -27,6 +27,7 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
  * Created by nazar.dovhyy on 09.07.2016.
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/restful/report")
 public class ReportController {
 
