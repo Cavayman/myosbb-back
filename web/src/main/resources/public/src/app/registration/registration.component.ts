0ff59d7c46cf365767235dab1a14d59fffc75fe1
Cavayman <cavayman@gmail.com>
2016-08-03 05:56:47 +0300

    registration and service

---
+++ web/src/main/resources/public/src/app/registration/registration.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit, Output} from '@angular/core'
+import {Router} from "@angular/router";
+import {EventEmitter} from '@angular/core';
+import {LoginStat} from "../../shared/services/login.stats";
+
+@Component({
+    selector: 'app-login',
+    templateUrl: 'src/app/registration/registration.html',
+    styleUrls:['assets/css/registration/registration.css']
+})
+export class RegistrationComponent implements OnInit {
+
+    isLoggedIn:boolean;
+    @Output() private loggedIn = new EventEmitter();
+
+    constructor(private _router:Router, private _loginStat:LoginStat) {
+    }
+
+    ngOnInit():any {
+
+    }
+
+    onUserLoginClick() {
+        this.isLoggedIn = true;
+        this.loggedIn.emit(this.isLoggedIn);
+        this._loginStat.setLoginStat(this.isLoggedIn);
+        this._router.navigate(['home']);
+
+    }
+
+}
---
+++ web/src/main/resources/public/src/app/user/users/users.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core'
+import {userItem} from "./userItem";
+import {HTTP_PROVIDERS, Http,Headers,Response} from "@angular/http";
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class UsersService {
+    private _pathUrl='http://localhost:52430/restful/user/';
+    constructor( private http:Http){
+    }
+
+    getAllUsers():Observable<any>{
+        console.log('in service');
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        return this.http.get(this._pathUrl,{headers:headers})
+            .map(response => response.json());
+    }
+
+    updateUser(user:userItem):Observable<userItem>{
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        return this.http.post(this._pathUrl+user.userId,JSON.stringify(user),{headers:headers})
+            .map((res:Response) => {return new userItem(res.json())});
+    }
+    deleteUser(user:userItem){
+        return this.http.delete(this._pathUrl+user.userId)
+            .map(response => response.json());
+    }
+}
