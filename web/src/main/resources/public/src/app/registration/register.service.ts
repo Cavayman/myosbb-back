5a1f5ad2de65daa42d9d48ffcecd0d5f16dff0c2
KostetskyRoma@yandex.ru <KostetskyRoma@yandex.ru>
2016-08-19 19:11:59 +0300

    revert register.service.ts

---
+++ web/src/main/resources/public/src/app/registration/register.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from "@angular/core";
+import {HTTP_PROVIDERS, Http,Headers,Response} from "@angular/http";
+import {User} from "../../shared/models/User";
+import {Observable} from 'rxjs/Observable';
+import ApiService = require("../../shared/services/api.service");
+
+@Injectable()
+export class RegisterService{
+
+    private _pathUrl=ApiService.serverUrl + '/registration';
+
+    constructor(private http:Http){
+    }
+    sendUser(user:User){
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        console.log(user);
+    return this.http.post(this._pathUrl,JSON.stringify(user),{headers:headers});
+    }
+
+    
+}
