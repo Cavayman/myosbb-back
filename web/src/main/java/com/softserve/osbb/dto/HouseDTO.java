7e021c85fda736d7f4247380f5202aed527fac92
NazDov <nazardovhuy@gmail.com>
2016-08-23 22:52:02 +0300

    LVJAVAAA-55 refactored HouseDTO using builder pattern

--- web/src/main/java/com/softserve/osbb/dto/HouseDTO.java
+++ web/src/main/java/com/softserve/osbb/dto/HouseDTO.java
@@ -1,7 +1,9 @@
 package com.softserve.osbb.dto;
 
-import com.softserve.osbb.model.House;
+import com.softserve.osbb.model.Apartment;
 import com.softserve.osbb.model.Osbb;
+
+import java.util.List;
 
 /**
  * Created by nazar.dovhyy on 03.08.2016.
@@ -15,36 +17,27 @@ public class HouseDTO {
     private String description;
     private String osbbName;
     private Integer apartmentCount;
+    private Integer numberOfInhabitants;
 
 
-    public HouseDTO(House house) {
-        if (house != null) {
-            this.houseId = house.getHouseId();
-            this.city = house.getCity();
-            this.street = house.getStreet();
-            this.zipCode = house.getZipCode();
-            this.description = house.getDescription();
-            this.apartmentCount = house.getApartments().size();
-        }
+    public HouseDTO(HouseDTOBuilder houseDTOBuilder) {
+        this.houseId = houseDTOBuilder.houseId;
+        this.city = houseDTOBuilder.city;
+        this.street = houseDTOBuilder.street;
+        this.zipCode = houseDTOBuilder.zipCode;
+        this.description = houseDTOBuilder.description;
+        this.osbbName = houseDTOBuilder.osbbName;
+        this.apartmentCount = houseDTOBuilder.apartmentCount;
+        this.numberOfInhabitants = houseDTOBuilder.numberOfInhabitants;
     }
-
 
     public Integer getHouseId() {
         return houseId;
     }
 
-
-    public void setHHData(Osbb osbb) {
-        if (osbb != null) {
-            this.osbbName = osbb.getName();
-        }
-    }
-
-
     public String getCity() {
         return city;
     }
-
 
     public String getStreet() {
         return street;
@@ -54,28 +47,101 @@ public class HouseDTO {
         return zipCode;
     }
 
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public String getOsbbName() {
         return osbbName;
     }
 
-    public void setOsbbName(String osbbName) {
-        this.osbbName = osbbName;
-    }
-
     public Integer getApartmentCount() {
         return apartmentCount;
     }
 
-    public void setApartmentCount(Integer apartmentCount) {
-        this.apartmentCount = apartmentCount;
+    public Integer getNumberOfInhabitants() {
+        return numberOfInhabitants;
+    }
+
+    public static class HouseDTOBuilder {
+        private Integer houseId;
+        private String city;
+        private String street;
+        private String zipCode;
+        private String description;
+        private String osbbName;
+        private Integer apartmentCount;
+        private Integer numberOfInhabitants;
+
+
+        public HouseDTOBuilder setHouseId(final Integer houseId) {
+            this.houseId = houseId;
+            return this;
+        }
+
+        public HouseDTOBuilder setCity(final String city) {
+            this.city = city;
+            return this;
+        }
+
+        public HouseDTOBuilder setStreet(final String street) {
+            this.street = street;
+            return this;
+        }
+
+        public HouseDTOBuilder setZipCode(final String zipCode) {
+            this.zipCode = zipCode;
+            return this;
+        }
+
+        public HouseDTOBuilder setDescription(final String description) {
+            this.description = description;
+            return this;
+        }
+
+        public HouseDTOBuilder setOsbbName(final Osbb osbb) {
+            if (osbb != null) {
+                this.osbbName = osbb.getName();
+            }
+            return this;
+        }
+
+        public HouseDTOBuilder setApartmentCount(final List<Apartment> apartments) {
+            if (apartments != null) {
+                this.apartmentCount = apartments.size();
+            }
+            return this;
+        }
+
+        public HouseDTOBuilder setNumberOfInhabitants(List<Apartment> apartments) {
+            int inhabitantsCount = 0;
+            if (apartments != null) {
+                for (Apartment apartment : apartments) {
+                    if (apartment.getUser() != null) {
+                        inhabitantsCount += 1;
+                    }
+                }
+            }
+            this.numberOfInhabitants = inhabitantsCount;
+            return this;
+        }
+
+        public HouseDTO build() {
+            return new HouseDTO(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "HouseDTO{" +
+                "houseId=" + houseId +
+                ", city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                ", description='" + description + '\'' +
+                ", osbbName='" + osbbName + '\'' +
+                ", apartmentCount=" + apartmentCount +
+                ", numberOfInhabitants=" + numberOfInhabitants +
+                '}';
     }
 }
