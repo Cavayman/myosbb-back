f5b22da0454cccc6cec7135042c627c633ce98fe
NazDov <nazardovhuy@gmail.com>
2016-08-23 22:43:21 +0300

    moving reusable constant fields to Constants class

--- persistence/src/main/java/com/softserve/osbb/utils/Constants.java
+++ persistence/src/main/java/com/softserve/osbb/utils/Constants.java
@@ -1,9 +1,13 @@
 package com.softserve.osbb.utils;
 
+import java.text.SimpleDateFormat;
+
 /**
  * Created by nazar.dovhyy on 20.08.2016.
  */
 public class Constants {
 
     public static final String DATE_FORMAT = "yyyy-MM-dd";
+    public static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat(DATE_FORMAT);
+    public static final String REPORTS = "reports";
 }
