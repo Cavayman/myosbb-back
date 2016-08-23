20f87f0120663ba09108d38b264683049ea3cfe0
NazDov <nazardovhuy@gmail.com>
2016-08-23 22:52:51 +0300

    LVJAVAAA-55 refactored HouseDTOMapper

--- web/src/main/java/com/softserve/osbb/dto/mappers/HouseDTOMapper.java
+++ web/src/main/java/com/softserve/osbb/dto/mappers/HouseDTOMapper.java
@@ -1,17 +1,30 @@
 package com.softserve.osbb.dto.mappers;
 
 import com.softserve.osbb.dto.HouseDTO;
+import com.softserve.osbb.model.Apartment;
 import com.softserve.osbb.model.House;
+
+import java.util.List;
 
 /**
  * Created by nazar.dovhyy on 03.08.2016.
  */
 public class HouseDTOMapper {
 
-
     public static HouseDTO mapHouseEntityToDTO(House house) {
-        HouseDTO houseDTO = new HouseDTO(house);
-        houseDTO.setHHData(house.getOsbb());
+        HouseDTO houseDTO = null;
+        if (house != null) {
+            houseDTO = new HouseDTO.HouseDTOBuilder()
+                    .setHouseId(house.getHouseId())
+                    .setCity(house.getCity())
+                    .setDescription(house.getDescription())
+                    .setStreet(house.getStreet())
+                    .setZipCode(house.getZipCode())
+                    .setOsbbName(house.getOsbb())
+                    .setApartmentCount((List<Apartment>) house.getApartments())
+                    .setNumberOfInhabitants((List<Apartment>) house.getApartments())
+                    .build();
+        }
         return houseDTO;
     }
 }
