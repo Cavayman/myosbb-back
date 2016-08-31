39dbdb89d833728e75f1126f7aa541b08240b714
Cavayman <cavayman@gmail.com>
2016-08-31 11:18:38 +0300

    Handle registration

--- web/src/main/java/com/softserve/osbb/controller/BasicController.java
+++ web/src/main/java/com/softserve/osbb/controller/BasicController.java
@@ -2,15 +2,15 @@ package com.softserve.osbb.controller;
 
 import com.softserve.osbb.model.User;
 import com.softserve.osbb.service.UserService;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resource;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.ServletException;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -29,29 +29,32 @@ public class BasicController {
     @Autowired
     UserService userService;
 
-    @RequestMapping(value = "user/login", method = RequestMethod.POST)
-    public String login(@RequestBody Map<String, String> json) throws ServletException {
-        if (json.get("username") == null || json.get("password") == null) {
-            throw new ServletException("Please fill in username and password");
-        }
-        String userName = json.get("username");
-        String password = json.get("password");
-
-        User user = userService.findUserByEmail(userName);
-        if (user == null) {
-            throw new ServletException("User name not found");
-        }
-        String pwd = user.getPassword();
-        if (!password.equals(pwd)) {
-            throw new ServletException("Invalid.Please check you'r username and password");
-        }
-        return Jwts.builder().setId(user.getUserId().toString()).setSubject(userName).claim("roles", "User").setIssuedAt(new Date())
-                .signWith(SignatureAlgorithm.HS256, "secretkey").compact();
-    }
+//    @RequestMapping(value = "user/login", method = RequestMethod.POST)
+//    public String login(@RequestBody Map<String, String> json) throws ServletException {
+//        if (json.get("username") == null || json.get("password") == null) {
+//            throw new ServletException("Please fill in username and password");
+//        }
+//        String userName = json.get("username");
+//        String password = json.get("password");
+//
+//        User user = userService.findUserByEmail(userName);
+//        if (user == null) {
+//            throw new ServletException("User name not found");
+//        }
+//        String pwd = user.getPassword();
+//        if (!password.equals(pwd)) {
+//            throw new ServletException("Invalid.Please check you'r username and password");
+//        }
+//        return Jwts.builder().setId(user.getUserId().toString()).setSubject(userName).claim("roles", "User").setIssuedAt(new Date())
+//                .signWith(SignatureAlgorithm.HS256, "secretkey").compact();
+//    }
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
     public User putUser(@RequestBody User user) {
+        if(userService.findUserByEmail(user.getEmail())!=null)
+            return userService.findUserByEmail(user.getEmail());
         logger.info("Saving user, sending to service");
         return userService.save(user);
     }
+
 
 }
