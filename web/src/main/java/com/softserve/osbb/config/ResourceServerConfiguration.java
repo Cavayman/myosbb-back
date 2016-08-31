c71ce6fdbdfc334dc6cc69bd895d38cea3a3a4c3
Cavayman <cavayman@gmail.com>
2016-08-31 12:58:16 +0300

    new security

---
+++ web/src/main/java/com/softserve/osbb/config/ResourceServerConfiguration.java
@@ -0,0 +1,45 @@
+package com.softserve.osbb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by cavayman on 30.08.2016.
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    private static final String RESOURCE_ID = "restservice";
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId(RESOURCE_ID);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http    .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/oauth/token").permitAll()
+                .antMatchers("/registration").permitAll()
+                .antMatchers("/restful/**").authenticated();
+
+    }
+
+
+
+
+
+}
